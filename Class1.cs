234edcfff7658057bbdd5d3b149ea7923f4860a5
Cristian Gerster <cristian_gerster@hotmail.com>
2021-04-29 13:11:47 -0300

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._1_U1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
